f195650ee3ccb1a27307f6513f557b5099971314
ict283 <glennlum@icloud.com>
2021-07-02 22:10:58 +0800

    Initial commit

---
+++ builderClass/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  builderClass
+//
+//  Created by Glenn Lum on 2/7/21.
+//  Copyright © 2021 Glenn Lum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
