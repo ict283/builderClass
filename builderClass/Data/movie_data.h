ba47032bc471bf56c443c2c15ede211fb58a3bdd
ict283 <glennlum@icloud.com>
2021-07-02 22:20:05 +0800

    Import source code

---
+++ builderClass/Data/movie_data.h
@@ -0,0 +1,11 @@
+
+using namespace std;
+
+//Some random test values
+
+string t[]= {"Pulp Finction", "Once Upon A Time...In Hollywood","Inglourious Basterds"};
+
+string s[] = {"In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters...","Rick, a washed-out actor, and Cliff, his stunt double, struggle to recapture fame and success in 1960s Los Angeles.","A few Jewish soldiers are on an undercover mission to bring down the Nazi government and put an end to the war."};
+
+double r[] = {8.9,7.6,8.3};
+
--- builderClass/main.cpp
+++ builderClass/main.cpp
@@ -1,15 +1,43 @@
-//
-//  main.cpp
-//  builderClass
-//
-//  Created by Glenn Lum on 2/7/21.
-//  Copyright © 2021 Glenn Lum. All rights reserved.
-//
+// For review purposes only! Does not represent the actual solution!
 
 #include <iostream>
+#include <list>
+#include "Data/movie_data.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+struct MovieType {
+    string title;
+    string synopsis;
+    double rating;
+};
+
+void print(MovieType& m);
+
+int main(void) {
+
+        list<MovieType> movie_database;
+    
+
+        for(int i = 0; i<3; i++) {
+            
+            MovieType m;
+            m.title = t[i];
+            m.synopsis = s[i];
+            m.rating = r[i];
+            
+            movie_database.push_back(m);
+        }
+
+        for(MovieType m : movie_database) {
+                print(m);
+        }
+
+        return 0;
+}
+
+void print(MovieType& m) {
+        cout<<"Title: "<<m.title<<endl;
+        cout<<"Synopsis: "<<m.synopsis<<endl;
+        cout<<"Rating: "<<m.rating<<endl<<endl;
 }
